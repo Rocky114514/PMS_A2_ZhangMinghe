c0fda4d4790b7e57937d475297b560dc1e778cd2
Rocky114514 <1522719807@qq.com>
2026-04-05 00:23:37 +0800

    feat: Part 2 - Successfully implemented global navigation and routing outlet

--- PROG2005_A2_ZhengMinghe/Part2_Angular_App/inventory-app/src/app/app.routes.ts
+++ PROG2005_A2_ZhengMinghe/Part2_Angular_App/inventory-app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ManageComponent } from './components/manage/manage.component';
+// ... 其他组件导入
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'manage', component: ManageComponent },
+  // ... 其他路由
+  { path: '', redirectTo: '/home', pathMatch: 'full' } // 默认进入首页
+];
